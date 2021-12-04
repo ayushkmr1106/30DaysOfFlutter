6d7efe1b0c8a0e60380aaf1498b6407ede2a3426
Ayush Kumar <shubham.verma144@gmail.com>
2021-12-04 15:33:31 +0530

    Day 7 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,108 +13,137 @@ class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool changeButton = false;
 
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if(_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+
+      await Future.delayed(const Duration(seconds: 1));
+      await  Navigator.pushNamed(context, MyRoute.homeRoute);
+
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/Startup_life.png',
-              fit: BoxFit.cover,
-              // height: 600.0,
-            ),
-            const SizedBox(height: 20.0,),
-            Text(
-              'Welcome $name',
-              style: const TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key : _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/hey_email.png',
+                // fit: BoxFit.cover,
+                // height: 600.0,
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "Username",
+              const SizedBox(height: 20.0,),
+              Text(
+                'Welcome $name',
+                style: const TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                      ),
+                      validator: (value) {
+                        if(value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {
+                        });
+                      },
                     ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                      validator: (value) {
+                        if(value!.isEmpty) {
+                          return "Password cannot be empty";
+                        }
+                        else if (value.length < 6) {
+                          return "Password lenght should be atleast 6";
+                        }
+                        return null;
+                      }
                     ),
-                  ),
-                  const SizedBox(
-                    height: 40.0,
-                  ),
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, MyRoute.homeRoute);
-                  //   }, 
-                  //   child: const Text("Login"),
-                  //   style: TextButton.styleFrom(
-                  //     minimumSize: const Size(150, 40)
-                  //   ),
-                  // ),
-                  // Gesture detector / inkwell
-                  
-                  InkWell(
-                    onTap: () async {
-                      // Navigator.pushNamed(context, MyRoute.homeRoute);
-                      setState(() {
-                        changeButton = true;
-                      });
-
-                      await Future.delayed(const Duration(seconds: 1), () {
-                        Navigator.pushNamed(context, MyRoute.homeRoute);
-                      });
-
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(seconds: 1),
-                      width: changeButton? 50:150,
-                      height: 50,
-                      alignment: Alignment.center,
-                      child: changeButton 
-                      ? const Icon(
-                        Icons.done, 
-                        color: Colors.white,
-                      ) 
-                      : const Text(
-                        "Login",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoute.homeRoute);
+                    //   }, 
+                    //   child: const Text("Login"),
+                    //   style: TextButton.styleFrom(
+                    //     minimumSize: const Size(150, 40)
+                    //   ),
+                    // ),
+                    // Gesture detector / inkwell
+                    
+                    Material(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          width: changeButton? 50:150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: changeButton 
+                          ? const Icon(
+                            Icons.done, 
+                            color: Colors.white,
+                          ) 
+                          : const Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
+                            ),
+                          ),
+                          // decoration: BoxDecoration(
+                          //   color: Colors.blue,
+                          //   // shape: changeButton
+                          //   //     ? BoxShape.circle
+                          //   //     : BoxShape.rectangle,
+                          //   borderRadius: BorderRadius.circular(changeButton ? 50 : 8)
+                          // ),
                         ),
                       ),
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        // shape: changeButton
-                        //     ? BoxShape.circle
-                        //     : BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(changeButton ? 50 : 8)
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       )
     );
